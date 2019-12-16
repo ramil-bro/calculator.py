5c57cdb6266d88e003223266ac0c0ba75e4dd246
Ramil <58654581+ramil-bro@users.noreply.github.com>
2019-12-16 13:00:04 +0400

    uyguwyvg.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+x = int(input('1-ci reqemi daxil et'))
+z=input('isare daxil edin')
+y = int(input('2-ci reqemi daxil edin'))
+if (z=='*'):
+  print(x*y)
+if (z=='+'):
+  print(x+y)
+if (z=='-'):
+  print(x-y)
+if (z=='/'):
+  print(x/y)
